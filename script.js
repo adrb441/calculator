831c67fea4b0e70c8ef97bb5ec8b9e4ef452c7ad
adrb441 <38795409+adrb441@users.noreply.github.com>
2023-05-01 19:25:44 -0700

    Add base operations functions

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+const x = 9;
+const y = 4;
+console.log(add(x,y));
+console.log(subtract(x,y));
+console.log(multiply(x,y));
+console.log(divide(x,y));
