d0d5778c087733e78c42aea33b4da48acfbdc5ac
adrb441 <38795409+adrb441@users.noreply.github.com>
2023-05-02 11:38:19 -0700

    Add functions to update display on button press

--- script.js
+++ script.js
@@ -14,9 +14,32 @@ function divide(x, y) {
     return x / y;
 }
 
-const x = 9;
+function buttonPressed(e) {
+    const input = e.target.textContent;
+    if (isNaN(input)) {
+        outputToScreen("");
+    } else {
+        outputToScreen(e.target.textContent);
+    }
+}
+
+function outputToScreen(output) {
+    const outputBox = document.querySelector('.output-box');
+    if (outputBox.textContent === "Calculator" || output === "") {
+        outputBox.textContent = output;
+    } else {
+        outputBox.textContent += output;
+    }
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', buttonPressed);
+});
+
+/*const x = 9;
 const y = 4;
 console.log(add(x,y));
 console.log(subtract(x,y));
 console.log(multiply(x,y));
-console.log(divide(x,y));
+console.log(divide(x,y));*/
